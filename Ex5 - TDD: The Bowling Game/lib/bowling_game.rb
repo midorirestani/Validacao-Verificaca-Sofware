220fbe0c96b777e52cb8ebccb9913099b3309121
Midori <midori.restani@gmail.com>
2020-09-30 02:01:19 -0300

    :white_check_mark: fixed spare test

--- Ex5 - TDD: The Bowling Game/lib/bowling_game.rb
+++ Ex5 - TDD: The Bowling Game/lib/bowling_game.rb
@@ -1,5 +1,46 @@
+require 'enumerator'
 class BowlingGame
-    def score()
-        return 0
+
+    def initialize()
+        @rolls = Array.new(21, 0)
+        @actual_roll = 0
+
+    end
+    
+    def roll(value)
+        @rolls[@actual_roll] = value
+        @actual_roll += 1
+    end
+
+    def get_score
+        score = 0
+        frame_index =0
+
+        for frame in 0..9
+                if is_spare?(frame_index)
+                    puts "is spare"
+                    score +=  (10 + spare_bonus(frame_index))
+                    frame_index += 2
+                else
+                    score +=  (@rolls[frame_index] + @rolls[frame_index+1])
+                    frame_index += 2
+                end
+            end
+        return score
+    end
+
+    def is_spare?(frame_index)
+        return (@rolls[frame_index]+@rolls[frame_index+1]) == 10
+    end
+
+    def spare_bonus(frame_index)
+        return @rolls[frame_index+2]
+    end
+
+    def print_rolls
+        for i in 0..@rolls.length
+            print "#{@rolls[i]} "
+        end
+        puts
     end
 end
--- Ex5 - TDD: The Bowling Game/spec/bowling_game_spec.rb
+++ Ex5 - TDD: The Bowling Game/spec/bowling_game_spec.rb
@@ -7,12 +7,33 @@ RSpec.describe BowlingGame do
             @bowling_game = BowlingGame.new
         end
 
-       context "when player got a gutter Game" do
+        context "gutter game" do
+            it "returns score equals 0" do
+                expect(@bowling_game.get_score).to be(0)
+            end
+        end
+        
+        context "all ones" do
+            it "returns score equals 20" do
+               roll_many(19, 1)
+                expect(@bowling_game.get_score).to be(20)
+            end
+        end
 
-           it "should score 0" do
-               expect(@bowling_game.score()).to be(0)
-           end
-       end
-       
+        context "one spare test" do
+            it "returns score equals 10 + next roll" do
+                @bowling_game.roll(7)
+                @bowling_game.roll(3)
+                @bowling_game.roll(5)
+
+                expect(@bowling_game.get_score).to be(20)
+            end
+        end
+    end
+
+    def roll_many(n, pins)
+        for i in 0..n
+            @bowling_game.roll(pins)
+        end
     end
 end
